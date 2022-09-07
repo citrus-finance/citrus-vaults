da75c3e465fddf7549ea396d457fa165ddfe7b87
Nickolas <101463700+0xnickolas@users.noreply.github.com>
2022-09-07 12:40:56 +0200

    BalancerPoolManager.sol finished and implemented

---
+++ src/harvesters/BalancerPoolManager.sol
@@ -0,0 +1,35 @@
+pragma solidity >=0.8.0;
+
+import "forge-std/Test.sol";
+import "solmate/tokens/ERC20.sol";
+
+import "../external/balancer-v2/IBalancerV2Vault.sol";
+import "../external/balancer-v2/IBalancerV2WeightedPool.sol";
+
+contract BalancerPoolManager {
+    function exitPool(address pool) public {
+        IBalancerV2WeightedPool balancerPool = IBalancerV2WeightedPool(pool);
+        IBalancerV2Vault balancerVault = IBalancerV2Vault(
+            balancerPool.getVault()
+        );
+        bytes32 poolId = balancerPool.getPoolId();
+
+        (address[] memory tokens, , ) = balancerVault.getPoolTokens(poolId);
+
+        uint256[] memory minAmountsOut = new uint256[](2);
+        IBalancerV2Vault.ExitPoolRequest memory request = IBalancerV2Vault
+            .ExitPoolRequest({
+                assets: tokens,
+                minAmountsOut: minAmountsOut,
+                userData: abi.encode(1, ERC20(pool).balanceOf(address(this))),
+                toInternalBalance: false
+            });
+
+        balancerVault.exitPool(
+            poolId,
+            address(this),
+            payable(address(this)),
+            request
+        );
+    }
+}
